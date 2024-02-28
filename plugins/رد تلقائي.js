71e8fbce0807c7f29638e424505bc95cd3910c0d
Lightobot <156609227+Lightobot@users.noreply.github.com>
2024-02-28 13:28:54 +0100

    Update رد تلقائي.js

--- plugins/رد تلقائي.js
+++ plugins/رد تلقائي.js
@@ -7,103 +7,24 @@ let handler = m => m;
 
    let chat = global.db.data.chats[m.chat]; 
    let responses; 
-   if (/^هلا$/i.test(m.text)) { 
+   if (/^نانا$/i.test(m.text)) { 
      responses = [ 
- '*ارحب*'  
+ '*نعمممم*'  
      ]; 
-} else if (/^السلام عليكم|سلام عليكم ورحمه الله وبركاته|سلام عليكم|السلام عليكم ورحمه الله وبركاته$/i.test(m.text)) { 
+    }else if (/^ون بيس$/i.test(m.text)) { 
      responses = [ 
-       '*وعليكم السلام*',  
-     ]; 
-   }else if (/^نيزوكو تشان|نيزوكو$/i.test(m.text)) { 
-     responses = [ 
-'*وش تريد🗿*'
-     ]; 
- }else if (/^شحالك|كيفك$/i.test(m.text)) { 
-     responses = [ 
-'*بخير دامك بخير*'
-     ]; 
-   }else if (/^نيزوكو_هل انت مرتبطة$/i.test(m.text)) { 
-     responses = [ 
-'*لا ولا أريد الارتباط🏌🏻‍♂💔*'
-   ]; 
-   }else if (/^تحبني|بوت $/i.test(m.text)) { 
-     responses = [ 
-'*مافيه بوت غيرك 🐦*',
-'*ورع🤭*',
-'*برااااااع 🔚*',
-]; 
-   }else if (/^تكرهني؟$/i.test(m.text)) { 
-     responses = [ 
-'*ماعاش من يكرهكك حبي 🙁*',
-'*لا بس لا تتعب نفسك لحبك🫥*',
-'*ااي اكرهك🙄*',   ]; 
-     
-     }else if (/^هاي|هالو$/i.test(m.text)) { 
-     responses = [ 
-       '*وعليكم السلام*',  
-
-     ]; 
-}else if (/^بحبك/i.test(m.text)) { 
-     responses = [ 
-       '*هممممممم*',  
-
-     ]; 
-   }else if (/^فلسطين$/i.test(m.text)) { 
-     responses = [ 
-'عاشششش تفهم'
-     ]; 
-   } else if (/^احبك$/i.test(m.text)) { 
-     responses = [ 
-'*ها توكل*'
-     ]; 
-     }else if (/^عامل ايه|عامل اي|عامل اية$/i.test(m.text)) { 
-     responses = [ 
-       'الحمدالله',  
-
+'*ونبيس عمك*'
      ];
-     }else if (/^تحبني$/i.test(m.text)) { 
+    }else if (/^لوفي$/i.test(m.text)) { 
      responses = [ 
-       'براااااااع',  
-
+'*لوفي عمك*'
      ];
-     }else if (/^وينه لبوت$/i.test(m.text)) { 
+    }else if (/^إسم$/i.test(m.text)) { 
      responses = [ 
-       'ها وش فيك معي؟',  
-
+'*اتشيغو*'
      ];
-     }else if (/^بوت|بوت$/i.test(m.text)) { 
-     responses = [ 
-       '*ها وش فيك معي*',  
-
-     ];
-     }else if (/^اهلا$/i.test(m.text)) { 
-     responses = [ 
-       '*🙂*',  
-
-     ]; 
-     }else if (/^مساء|مساء$/i.test(m.text)) { 
-     responses = [ 
-       'مساء الخير',  
-
-     ];
-     }else if (/^صباح|صباح$/ .test(m.text)) { 
-     responses = [ 
-       '*صباح الورد🧸*',  
-     ];
-       }else if (/^اوامر$/i.test(m.text)) { 
-     responses = [ 
-       '*لا تنسى ال .*',  
-     ];
-            }else if (/^Nezuko$/i.test(m.text)) { 
-     responses = [ 
-       '*تانجيرو☹️*',  
-     ];
-            }else if (/^مرحبا$/i.test(m.text)) { 
-     responses = [ 
-       '*مرحبا🧸*',  
-     ];
-   }
+    
+  }
    if (responses) { 
      let randomIndex = Math.floor(Math.random() * responses.length); 
      conn.reply(m.chat, responses[randomIndex], m); 
